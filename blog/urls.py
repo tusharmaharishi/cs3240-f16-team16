ef96d53479424bdbb3a56112213f749d3863fbcd
Tushar Maharishi <tm5gf@virginia.edu>
2016-10-30 22:46:59 -0400

    added different pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), #views.post_list is the place to go if we arrive at the main page
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
 
