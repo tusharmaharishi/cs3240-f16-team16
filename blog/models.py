ba8d3bd89d391a3e00dfaef6373be526b6d63737
Tushar Maharishi <tm5gf@virginia.edu>
2016-10-30 20:51:05 -0400

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model): #this post is a django model
+    author = models.ForeignKey('auth.User') #link to another model
+    title = models.CharField(max_length=200) #text with limited number of chars
+    text = models.TextField() #text with unlimited number of chars
+    created_date = models.DateTimeField(default=timezone.now) #date and time
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
