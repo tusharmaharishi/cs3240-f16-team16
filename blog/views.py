02119875a0c74d9b4cfb3b973c90463d4f9ace45
Tushar Maharishi <tm5gf@virginia.edu>
2016-11-21 13:18:23 -0500

    added Q

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def post_list(request):
     #posts = Post.objects.all()#filter(published_date__lte=timezone.now()).order_by('published_date')
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #post = get_object_or_404(Post, pk=pk)
-    posts = Post.objects.filter((Q(private=False) | Q(author=request.user)) & Q(published_date__lte=timezone.now())).order_by('published_date')
+    posts = Post.objects.filter(Q(Q(private=False) | Q(author=request.user)) & Q(published_date__lte=timezone.now())).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
