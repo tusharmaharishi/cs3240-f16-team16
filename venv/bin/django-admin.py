e65e322d27549f88507aff00dc26c0e88897ae0d
Tushar Maharishi <tm5gf@virginia.edu>
2016-11-26 16:16:59 -0500

    Added files and changes for heroku

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tusharmaharishi/DjangoProjs/dropbox/cs3240-f16-team16/venv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
