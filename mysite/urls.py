0f602c75fea6dde4fa0b84d67d6b566b215df2b9
Tushar Maharishi <tm5gf@virginia.edu>
2016-11-26 16:27:55 -0500

    Added files and changes for Heroku deployment.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import include, url, patterns
+from django.conf.urls import include, url #, patterns
 from  django.contrib.auth import views
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
+    #patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -14,4 +15,4 @@ urlpatterns = patterns('',
     url(r'', include('blog.urls')),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
