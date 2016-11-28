65bd5ffd127c4cb6beb72c5fddbc2cbbff45e9fc
Kevin <kwa6nf@virginia.edu>
2016-11-27 21:22:45 -0500

    Able to message users

--- blog/urls.py
+++ blog/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     url(r'^folder/new/$', views.folder_new, name='folder_new'),
     url(r'^folder/$', views.folder_list, name='folder_list'),
     url(r'^folder/(?P<pk>\d+)/$', views.folder_detail, name='folder_detail'),
+        url(r'^messages/', include('django_messages.urls')),
     #url(r'^group/(?P<pk>\d+)/add/$', views.add_user, name='add_user'),
 ]
 
