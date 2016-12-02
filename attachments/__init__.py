e8c475ab2be1774855ab9ebab68ceed1683b67fc
Kevin <kwa6nf@virginia.edu>
2016-12-02 10:32:33 -0500

    Able to view multiple files from table

--- attachments/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'attachments.apps.AttachmentsConfig'
--- attachments/admin.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-from attachments.models import Attachment
-from django.contrib.contenttypes.admin import GenericStackedInline
-
-class AttachmentInlines(GenericStackedInline):
-    model = Attachment
-    exclude = ()
-    extra = 1
