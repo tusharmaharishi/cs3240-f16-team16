8e2e5e0d836934e6ff41a76328c0bff61ea059d5
Kevin <kwa6nf@virginia.edu>
2016-12-04 18:54:47 -0500

    fixed notification function

--- blog/static/js/badge.js
+++ blog/static/js/badge.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
 	$.ajax("/get_unread_messages", {
 		method: 'GET',
 		success: function(response){
-			console.log(response);
-			$('#unread_messages').text(response);
+			if(response != 0) {
+				console.log(response);
+				$('#unread_messages').text(response);
+			} else {
+				$('#unread_messages').text("");
+			}
 		}
 	});
 });
